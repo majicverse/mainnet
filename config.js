82f078cb99aafd003d6cadde28310f92605857fc
th <1450096592@qq.com>
2022-05-07 23:30:29 +0800

    production

---
+++ config.js
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const config = {
+  network: {
+    chainId: "0x38",
+    chainName: "Binance Smart Chain",
+    nativeCurrency: {
+      name: "BNB",
+      symbol: "BNB",
+      decimals: 18,
+    },
+    rpcUrls: ["https://bsc-dataseed.binance.org/"],
+    blockExplorerUrls: ["https://bscscan.com/"],
+  },
+  captainContracts: "0xcD83733E17713cE185344F7308B329A3bbCe8d9F", // 舰长
+  majicArkContracts: "0x2892FEd79baac81977aA2DF92CFF4285EB18275e", // 马吉
+  seaOfFreedomContracts: "0x025e77529E5664ef241C0D9F19568c6fA43a8Aba", // 自由
+};
