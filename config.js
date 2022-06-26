7eb9dfa2551da9aeca9ba8bf11c9338543d93eb5
th <1450096592@qq.com>
2022-06-26 23:41:38 +0800

    update base

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+//hubToken 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
 const config = {
   network: {
     chainId: "0x38",
@@ -11,7 +12,8 @@ const config = {
     rpcUrls: ["https://bsc-dataseed.binance.org/"],
     blockExplorerUrls: ["https://bscscan.com/"],
   },
-  captainContracts: "0xcD83733E17713cE185344F7308B329A3bbCe8d9F", // 舰长
+  // captainContracts: "0xcD83733E17713cE185344F7308B329A3bbCe8d9F", // 舰长
+  captainContracts: "0x1e2a5ACbc6391bA44bfB5501b61958691e937ed9", // 舰长
   majicArkContracts: "0x2892FEd79baac81977aA2DF92CFF4285EB18275e", // 马吉
   seaOfFreedomContracts: "0x025e77529E5664ef241C0D9F19568c6fA43a8Aba", // 自由
 };
